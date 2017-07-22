050caf0a9f32cc3341e708a3c70b9ac78cc9984f
Travis <yiutingtang@gmail.com>
2017-07-22 23:52:31 +0800

    Changed some styles

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
     return (
 	<ScrollableTabView
 	style = {{marginTop: 20,}}
-	initialPage = {1}
+	initialPage = {0}
 	renderTabBar = {() => <FacebookTabBar/>}
 
 	>
--- components/TaskOverviewCard.js
+++ components/TaskOverviewCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {styles} from '../Style'
-import {Text,View,StyleSheet} from 'react-native';
+//import {Text,View,StyleSheet} from 'react-native';
+import {Text,View} from 'react-native-ui-lib';
+import {StyleSheet} from 'react-native'
 
 
 export default class TaskOverviewCard extends React.Component{
@@ -15,18 +17,37 @@ export default class TaskOverviewCard extends React.Component{
 		console.log("**");
 		let isTrainTask = Object.keys(this.task).indexOf('config') != -1;
 		var separatorColor = isTrainTask?"lightpink":"palegreen";
+		var taskTypeShorthand = isTrainTask?"T":"P";
 		return (
 			<View style = {styles.card}>
 				<View style={{flex:1,borderRightWidth: 3,borderRightColor: separatorColor}}>
-				
+					<Text text20 style={{color: separatorColor,paddingLeft:5,alignContent: "center",flex:1}}>{taskTypeShorthand}</Text>	
 				</View>
 
-				<View style={{flex:4,borderLeftWidth:5,borderLeftColor: "white"}}>
-					<Text>{this.task.name}</Text>
+				<View style={cardStyle.midSegment}>
+					<Text text50>{this.task.name}</Text>
+					<Text text80 style = {{color: "gray"}}>From model  "{this.task.weight.model.name}"</Text>
 				</View>
-				<View style={{flex:1}}></View>
+				<View style={cardStyle.rightSegment}></View>
 			</View>
 		)
 	}
 	
 };
+
+const cardStyle = StyleSheet.create({
+	leftSegment: {
+
+	},
+
+	midSegment: {
+		flex:4,
+		borderLeftWidth:5,
+		borderTopWidth:2,
+		borderColor: "white"
+	},
+
+	rightSegment: {
+		flex: 1
+	}
+});
